10ce20bb73b3ed1ed74459feaf1b5f3caea86c32
eunho <76768140sa@gmail.com>
2025-12-26 18:01:46 +0900

    중간 저ì

---
+++ src/main/java/com/example/demo/entity/Memo.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tbl_memo")
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memo;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
+}
